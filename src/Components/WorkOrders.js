983f73b3a7ead6b841925da7dd564c3e11dc183b
Bethany Alcamo <balcamo@verawaterandpower.com>
2019-11-22 12:18:25 -0800

    working on dropdowns on work orders
    not currently working for setting a value

--- src/Components/WorkOrders.js
+++ src/Components/WorkOrders.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-//import fetch from 'isomorphic-fetch';
-import { Input } from 'reactstrap';
+import React, {Component, useState } from 'react';
+import fetch from 'isomorphic-fetch';
+import { InputGroupButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 
 class WorkOrders extends Component {
@@ -11,26 +11,43 @@ class WorkOrders extends Component {
           setState:false,
           orderFrom:Number,
           orderTo:Number,
+          dropdownOpen:false,
+          setDropdownOpen:false
         };
+        this.toggleDropDown = this.toggleDropDown.bind(this);
+
+      }
+      toggleDropDown(){
+          this.setState({dropdownOpen: !this.state.dropdownOpen});
       }
       ChangeinState(){
           //fetch for workorder numbers
+          console.log(this.state.selectedState);
       }
       render(){
-
         return (
             <div>
                 <header >
                     <h1>Work Orders</h1>
                 </header>
                 <div>
-                    <Input type="select"  value={this.state.selectedState} onChange={(e)=> this.setState({ selectedState: e.target.value }), this.ChangeinState()}>
-                        <option value="null"> --- </option>
-                        <option value="void"> Void </option>
-                        <option value="not started"> Not Started </option>
-                        <option value="in progress"> In Progress </option>
-                        <option value="closed"> Closed </option>
-                    </Input>
+                    <InputGroupButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown} value={this.state.selectedState} onChange={(e)=> this.setState({ selectedState: e.target.value }), this.ChangeinState()}>
+                        <DropdownToggle caret>
+                            Button Dropdown
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem header value="null">Status options</DropdownItem>
+                            <DropdownItem value="new">New</DropdownItem>
+                            <DropdownItem value="not started">Not Started</DropdownItem>
+                            <DropdownItem value="in progress" > In Progress</DropdownItem>
+                            <DropdownItem value="completed" > Completed</DropdownItem>
+                            <DropdownItem value="closed">Closed</DropdownItem>
+                            <DropdownItem value="void">Void</DropdownItem>
+                        </DropdownMenu>
+                    </InputGroupButtonDropdown>
+                </div>
+                <div>
+
                 </div>
             </div>
         )
