7aa8da01a8059354a5740cbe9276b6511a05ae24
Bethany Alcamo <balcamo@verawaterandpower.com>
2019-11-25 10:00:24 -0800

    status selector is now triggering get

--- src/Components/WorkOrders.js
+++ src/Components/WorkOrders.js
@@ -1,14 +1,17 @@
 import React, {Component, useState } from 'react';
 import fetch from 'isomorphic-fetch';
-import { InputGroupButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Input } from 'reactstrap';
+import { watchFile } from 'fs';
 
 
 class WorkOrders extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          selectedState: null,
+          baseURL:'https://apiarydev-ispringbrook.azurewebsites.net/api/workorder/',
+          selectedState: 'Status options',
           setState:false,
+          workOrderNums:[],
           orderFrom:Number,
           orderTo:Number,
           dropdownOpen:false,
@@ -20,9 +23,38 @@ class WorkOrders extends Component {
       toggleDropDown(){
           this.setState({dropdownOpen: !this.state.dropdownOpen});
       }
-      ChangeinState(){
-          //fetch for workorder numbers
-          console.log(this.state.selectedState);
+      handleNewStat(newStatus){
+        setTimeout(() => {
+            this.setState({selectedState:newStatus});
+            console.log('in wait '+this.state.selectedState);
+            this.ChangeInStatus();
+        }, 1000);
+        
+        
+      }
+      ChangeInStatus(){
+        console.log('in change function '+this.state.selectedState);
+        fetch(this.state.baseURL+this.state.selectedState, {
+            method:"GET"
+        }).then(function(response) {
+            if (response.ok) {
+            return response
+            } else {
+            var error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+            //var tempArr = data;
+            //tempArr.sort(function(a, b){return a - b});
+            this.setState({ workOrderNums: data })
+            })
+            .catch(console.log);
+        console.log(this.state.workOrderNums);
+        
       }
       render(){
         return (
@@ -31,20 +63,16 @@ class WorkOrders extends Component {
                     <h1>Work Orders</h1>
                 </header>
                 <div>
-                    <InputGroupButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown} value={this.state.selectedState} onChange={(e)=> this.setState({ selectedState: e.target.value }), this.ChangeinState()}>
-                        <DropdownToggle caret>
-                            Button Dropdown
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem header value="null">Status options</DropdownItem>
-                            <DropdownItem value="new">New</DropdownItem>
-                            <DropdownItem value="not started">Not Started</DropdownItem>
-                            <DropdownItem value="in progress" > In Progress</DropdownItem>
-                            <DropdownItem value="completed" > Completed</DropdownItem>
-                            <DropdownItem value="closed">Closed</DropdownItem>
-                            <DropdownItem value="void">Void</DropdownItem>
-                        </DropdownMenu>
-                    </InputGroupButtonDropdown>
+                    <Input type="select" value={this.state.selectedState} onChange={(e)=> this.handleNewStat(e.target.value)} >
+                        <option value='Status options'>Status options</option>
+                        <option value='New'>New</option>
+                        <option value='Not Started'>Not Started</option>
+                        <option value='In Progress'>In Progress</option>
+                        <option value='Completed'>Completed</option>
+                        <option value='Closed'>Closed</option>
+                        <option value='Void'>Void</option>
+
+                    </Input>
                 </div>
                 <div>
 
