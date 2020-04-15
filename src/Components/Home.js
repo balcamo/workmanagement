604d0774a516a4f1d6ca2cbb1b39aa481d1eff12
Bethany Alcamo <balcamo@verawaterandpower.com>
2020-04-15 07:38:25 -0700

    addind css for buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
+import * as urls from '../urlsConfig';
 
 function Home() {
     return (
         <div>
             <Breadcrumb className="Breadcrumbs">
-                <BreadcrumbItem >Apiary</BreadcrumbItem>
-                <BreadcrumbItem >Work Management</BreadcrumbItem>
+                <BreadcrumbItem ><a href={urls.homePage}>Apiary</a></BreadcrumbItem>
+                <BreadcrumbItem active >Work Management</BreadcrumbItem>
             </Breadcrumb>
                 
             <h1>Work Management</h1>
             
-            <Button size="lg" className="Breadcrumbs" >
-                <a href="/workOrders">Work Orders</a>
-            </Button>
+            <Button size="lg" className="homeButtons" href="/workOrders">Work Orders</Button>
         </div>
     )
 }
